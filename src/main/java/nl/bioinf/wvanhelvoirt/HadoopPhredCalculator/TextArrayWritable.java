89264c5b43888ffbed33c0aa3fce27e14aa05a55
Wout van Helvoirt <wout.van.helvoirt@gmail.com>
2016-06-18 23:11:43 +0200

    cleanup

--- src/main/java/nl/bioinf/wvanhelvoirt/HadoopPhredCalculator/TextArrayWritable.java
+++ src/main/java/nl/bioinf/wvanhelvoirt/HadoopPhredCalculator/TextArrayWritable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Lonneke Scheffer and Wout van Helvoirt
+ * Copyright (c) 2016 Wout van Helvoirt
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
